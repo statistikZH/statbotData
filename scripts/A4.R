e553034f3ff0c67954aca4fe4fd8f271723f78cb
philbosch <philippmartinbosch@gmail.com>
2023-07-18 13:05:15 +0200

    minor changes to pipeline of ds_a4 (energiebilanz schweiz)

--- scripts/A4.R
+++ scripts/A4.R
@@ -17,14 +17,13 @@ spatial_unit_df <- readr::read_csv("data/const/spatial_unit_postgres.csv")
 ds$data %>%
   janitor::clean_names() %>%
   tibble::as_tibble() %>%
-  dplyr::mutate(raeumliche_ebene = "Schweiz") %>%
   dplyr::rename("beobachtungseinheit" = rubrik,
-                "anzahl_terajoule" = tj) -> ds$data
+                "anzahl" = tj) -> ds$data
 
 # join the cleaned data to the postgres spatial units table ---------------
 
 ds$data <- ds$data %>%
-  dplyr::left_join(dplyr::select(spatial_unit_df, spatialunit_uid, name_de), by = c("raeumliche_ebene" = "name_de"))
+  dplyr::mutate(spatialunit_uid = spatial_mapping_country())
 
 ## check that each spatial unit could be matched -> this has to be TRUE
 
@@ -33,3 +32,4 @@ assertthat::noNA(ds$data$spatialunit_uid)
 
 # ingest into postgres ----------------------------------------------------
 
+### important: name the table as energiebilanz_schweiz_in_tera_joule
