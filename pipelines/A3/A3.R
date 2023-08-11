5d79f2463f1022cc3db8763ab9e7976679b61cda
Sabine Maennel <sabine.maennel@gmail.com>
2023-08-11 14:40:06 +0200

    standardize pipeline for A3
    add first query and fill metadata table

---
+++ pipelines/A3/A3.R
@@ -0,0 +1,45 @@
+# -------------------------------------------------------------------------
+# Step: Get the data
+# input: google sheet
+# output: ds$data, ds$dir
+# -------------------------------------------------------------------------
+
+ds <- create_dataset(id = "A3")
+ds <- download_data(ds)
+ds
+
+# -------------------------------------------------------------------------
+# Step: Clean the data and add spatial unit
+#   input: ds$data
+#   output: ds$postgres_export: spatial unit uid added
+# -------------------------------------------------------------------------
+
+# clean excel-artifacts and add "beobachtungseinheit" as well as
+# spatialunit_id for switzerland
+ds$postgres_export <- ds$data %>%
+  tidyr::drop_na() %>%
+  dplyr::rename(jahr = 1, anzahl = 2) %>%
+  dplyr::rename(anzahl = "anzahl_millionen_tonnen_co2_equivalent") %>%
+  dplyr::mutate(spatialunit_uid = spatial_mapping_country())
+ds$postgres_export
+
+# -------------------------------------------------------------------------
+# Step: Testrun queries on sqllite
+#   input: ds$postgres_export, ds$dir/queries.sql
+#   output: ds$dir/queries.log
+# -------------------------------------------------------------------------
+
+statbotData::testrun_queries(
+  ds$postgres_export,
+  ds$dir,
+  ds$name
+)
+
+# -------------------------------------------------------------------------
+# Step: Write metadata tables
+#   input: ds$postgres_export
+#   output: ds$dir/metadata_tables.csv
+#           ds$dir/metadata_table_columns.csv
+# -------------------------------------------------------------------------
+
+read_write_metadata_tables(ds)
