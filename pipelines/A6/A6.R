6e692d06e1f261ea09d42609acad81e535a75aee
Sabine Maennel <sabine.maennel@gmail.com>
2023-08-13 11:45:51 +0200

    pipeline A6: standardize pipeline
    - complete metadata
    - rerun queries to adapt layout of the result log
    - add sample

--- pipelines/A6/A6.R
+++ pipelines/A6/A6.R
@@ -32,11 +32,11 @@ ds$postgres_export <- ds$data %>%
   dplyr::mutate(
     jahr = as.numeric(stringr::str_extract(jahr, "\\d+"))
   )
-ds$postgres_export
+unique(ds$postgres_export$jahr)
 
 # -------------------------------------------------------------------------
 # Step: Testrun queries on sqllite
-#   input: ds$postgres_export, ds$dir/queries.sql
+#   input:  ds$postgres_export, ds$dir/queries.sql
 #   output: ds$dir/queries.log
 # -------------------------------------------------------------------------
 
@@ -48,10 +48,11 @@ statbotData::testrun_queries(
 
 # -------------------------------------------------------------------------
 # Step: Write metadata tables
-#   input: ds$postgres_export
+#   input:  ds$postgres_export
 #   output: ds$dir/metadata_tables.csv
 #           ds$dir/metadata_table_columns.csv
+#           ds$dir/sample.csv
 # -------------------------------------------------------------------------
 
 read_write_metadata_tables(ds)
-
+dataset_sample(ds)
