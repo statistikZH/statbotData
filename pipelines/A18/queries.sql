fea7212006d82921ac124235ac89fe7c8be9eb2c
av1611 <alexej.ashkenazi@gmail.com>
2023-08-21 17:19:01 +0200

    upd

--- pipelines/A18/queries.sql
+++ pipelines/A18/queries.sql
@@ -1,2 +1,6 @@
 -- What is the number of lines in the database?
 SELECT COUNT(*) FROM ausgewaehlte_variablen_nach_staedten_und_agglomerationen;
+
+-- What is the average of the value variable
+SELECT AVG(value)
+FROM ausgewaehlte_variablen_nach_staedten_und_agglomerationen;
