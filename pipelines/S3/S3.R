fb9d6760e6ae43d69f4a1edeb04f483859ce4b34
Cyril Matthey-Doret <cmdoret@mailbox.org>
2023-08-28 13:47:59 +0200

    Queries s3 (#68)
    * feat(S3): include both COFOG classification levels
    
    * fix(S3): update metadata table format
    
    * chore(S3): regen sample+log
    
    * feat(S3): add first queries
    
    * fix(S3): quoting in table metadata
    
    Co-authored-by: Sabine Maennel <sabine.maennel@gmail.com>
    
    ---------
    
    Co-authored-by: Sabine Maennel <sabine.maennel@gmail.com>

--- pipelines/S3/S3.R
+++ pipelines/S3/S3.R
@@ -13,20 +13,35 @@ ds$query <- '
 PREFIX schema: <http://schema.org/>
 PREFIX cofog: <https://environment.ld.admin.ch/foen/BFS_cofog_national/>
 PREFIX cube: <https://cube.link/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 
-
-SELECT ?period ?cofog ?sector ?mio_chf_expenditure ?percent_total_expenditure
+SELECT ?period ?cofog_narrow ?cofog_broad ?sector ?mio_chf_expenditure ?percent_total_expenditure
 WHERE {
   cofog:1 cube:observationSet ?observationSet0 .
-  ?observationSet0 cube:observation ?source0 .
-  ?source0 cofog:cofog [ schema:name ?cofog ] .
-  ?source0 cofog:period ?period .
-  ?source0 cofog:in-pct ?percent_total_expenditure .
-  ?source0 cofog:in-mio-chf ?mio_chf_expenditure .
-  ?source0 cofog:sector [ schema:name ?sector ] .
 
+  ?observationSet0 cube:observation ?source0 .
+
+  ?source0
+    cofog:cofog ?cofog_narrow_uri ;
+    cofog:period ?period ;
+    cofog:in-pct ?percent_total_expenditure ;
+    cofog:in-mio-chf ?mio_chf_expenditure ;
+    cofog:sector [ schema:name ?sector ] .
+
+  # COFOG has multiple levels of classification, here we use 2 (narrowest and one above)
+  ?cofog_narrow_uri
+    schema:name ?cofog_narrow ;
+    skos:broader ?cofog_broad_uri .
+
+  ?cofog_broad_uri schema:name ?cofog_broad .
+
+  # Ensure that cofog_narrow is the most specific cofog classification available
+  FILTER NOT EXISTS { ?x skos:broader ?cofog_narrow_uri } .
+
+  # Only include german language data
   FILTER(LANG(?sector) = "de")
-  FILTER(LANG(?cofog) = "de")
+  FILTER(LANG(?cofog_broad) = "de")
+  FILTER(LANG(?cofog_narrow) = "de")
 }
 '
 ds <- download_data(ds)
@@ -34,23 +49,29 @@ ds <- download_data(ds)
 # -------------------------------------------------------------------------
 # Step: Clean the data
 #   input:  ds$data
-#.  output: ds$postgres_export:
+# .  output: ds$postgres_export:
 #           - clean columns names
-#.          - rename data columns
-#.          - map spatial unit (only country exists as spatial unit)
+# .          - rename data columns
+# .          - map spatial unit (only country exists as spatial unit)
 # -------------------------------------------------------------------------
+
+# cofog_narrow and cofog_broad are known as groups and divisions, respectively
+# Source: https://ec.europa.eu/eurostat/statistics-explained/index.php?title=Glossary:Classification_of_the_functions_of_government_(COFOG)
+
 
 ds$postgres_export <- ds$data %>%
   janitor::clean_names() %>%
   dplyr::rename(
     jahr = period,
-    aufgabenbereich_des_staates = cofog,
+    aufgabenbereich_cofog_gruppen = cofog_narrow,
+    aufgabenbereich_cofog_teilung = cofog_broad,
     institutioneller_sektor = sector,
     ausgaben_in_mio_chf = mio_chf_expenditure,
     prozent_der_gesamtausgaben = percent_total_expenditure
   ) %>%
   # add CH as spatial unit
   dplyr::mutate(spatialunit_uid = statbotData::spatial_mapping_country())
+
 
 # -------------------------------------------------------------------------
 # Step: Testrun queries on sqllite
@@ -69,7 +90,7 @@ statbotData::testrun_queries(
 #   input:  ds$postgres_export
 #   output: ds$dir/metadata_tables.csv
 #           ds$dir/metadata_table_columns.csv
-#.          ds$dir/sample.csv
+#           ds$dir/sample.csv
 # -------------------------------------------------------------------------
 
 read_write_metadata_tables(ds)
--- pipelines/S3/queries.sql
+++ pipelines/S3/queries.sql
@@ -1,6 +1,84 @@
--- Welcher Sektor hatte in 2020 die hoechsten Staatsausgaben?
-SELECT T.aufgabenbereich_des_staates as ausgaben_bereich_mit_den_hoechsten_ausgaben_2020
-FROM staatsausgaben_nach_aufgabenbereichen_cofog as T
-JOIN spatial_unit as S on T.spatialunit_uid = S.spatialunit_uid
-WHERE S.name_de='Schweiz' AND S.country=TRUE AND T.jahr=2020
-ORDER BY ausgaben_in_mio_chf DESC LIMIT 1;
+-- Welche Teilung hatte im Jahr 2020 die höchsten Staatsausgaben?
+SELECT aufgabenbereich_cofog_teilung as ausgaben_bereich_mit_den_hoechsten_ausgaben_2020
+FROM staatsausgaben_nach_aufgabenbereichen_cofog
+WHERE institutioneller_sektor="Staat" AND jahr=2020
+GROUP BY aufgabenbereich_cofog_teilung
+ORDER BY SUM(ausgaben_in_mio_chf) DESC LIMIT 1;
+
+-- Welches waren die 3 grössten Ausgabenquellen des Bundes im Jahr 2021 und wie hoch waren diese?
+SELECT SUM(ausgaben_in_mio_chf),aufgabenbereich_cofog_gruppen as ausgaben_bereich_mit_den_hoechsten_ausgaben_2020
+FROM staatsausgaben_nach_aufgabenbereichen_cofog
+WHERE institutioneller_sektor="Bund" AND jahr=2020
+GROUP BY aufgabenbereich_cofog_gruppen
+ORDER BY SUM(ausgaben_in_mio_chf) DESC LIMIT 3;
+
+-- Wie viel Prozent des Staat Budgets gab im Jahr 2021 fur Bildung aus?
+SELECT SUM(prozent_der_gesamtausgaben) as prozent_der_gesamtausgaben_fur_bildung
+FROM staatsausgaben_nach_aufgabenbereichen_cofog
+WHERE institutioneller_sektor="Staat"
+    AND jahr=2021
+    AND aufgabenbereich_cofog_teilung = "Bildungswesen";
+
+-- Wie hoch waren die religionsbezogenen Ausgaben der Kantone im Jahr ihres Höchststandes?
+SELECT t1.jahr, t1.aufgabenbereich_cofog_gruppen, t1.ausgaben_in_mio_chf
+FROM staatsausgaben_nach_aufgabenbereichen_cofog t1
+INNER JOIN (
+    SELECT jahr, SUM(ausgaben_in_mio_chf) AS tot_spending
+    FROM staatsausgaben_nach_aufgabenbereichen_cofog
+    WHERE LOWER(aufgabenbereich_cofog_gruppen) LIKE '%religion%' AND institutioneller_sektor = 'Kantone'
+    GROUP BY jahr
+    ORDER BY tot_spending DESC LIMIT 1
+) t2 ON t1.jahr = t2.jahr
+WHERE t1.aufgabenbereich_cofog_gruppen LIKE '%religion%' AND t1.institutioneller_sektor = 'Kantone';
+
+-- Wie viel hat jeder institutionelle Sektor im Jahr 2018 für den Umweltschutz ausgegeben?
+SELECT SUM(ausgaben_in_mio_chf), institutioneller_sektor
+FROM staatsausgaben_nach_aufgabenbereichen_cofog
+WHERE jahr=2018 AND aufgabenbereich_cofog_teilung = "Umweltschutz"
+GROUP BY institutioneller_sektor
+ORDER BY SUM(ausgaben_in_mio_chf) ASC;
+
+-- Welcher Anteil des Schweizer Budgets wird jedes Jahr in Sicherheit investiert?
+SELECT jahr, SUM(prozent_der_gesamtausgaben)
+FROM staatsausgaben_nach_aufgabenbereichen_cofog
+WHERE LOWER(aufgabenbereich_cofog_gruppen) LIKE "%sicherheit%"
+    AND institutioneller_sektor = "Staat"
+GROUP BY jahr
+ORDER BY jahr ASC;
+
+-- Wie hoch waren die Mittel für die Polizei im Vergleich zu denen für die Feuerwehr im Jahr 2020?
+SELECT ausgaben_in_mio_chf, aufgabenbereich_cofog_gruppen
+FROM staatsausgaben_nach_aufgabenbereichen_cofog
+WHERE (
+    LOWER(aufgabenbereich_cofog_gruppen) LIKE "%polizei%"
+    OR LOWER(aufgabenbereich_cofog_gruppen) LIKE "%feuerwehr%"
+)
+    AND jahr = 2020
+    AND institutioneller_sektor = "Staat";
+
+-- Wie hoch war der Anteil des Gemeinde Budgets, der seit 2011 für Kommunikation bereitgestellt wurde?
+SELECT jahr, SUM(prozent_der_gesamtausgaben) as prozent_der_gesamtausgaben_fur_kommunikation_in_landgemeinden
+FROM staatsausgaben_nach_aufgabenbereichen_cofog
+WHERE LOWER(aufgabenbereich_cofog_gruppen) LIKE "%kommunikation%"
+    AND institutioneller_sektor = "Gemeinden"
+    AND jahr >= 2011
+GROUP BY jahr
+ORDER BY jahr ASC;
+
+-- Wie hoch ist der Anteil des Schweizer Budgets, der 2010 und 2020 für die Gesundheit ausgegeben wird?
+SELECT jahr, SUM(prozent_der_gesamtausgaben)
+FROM staatsausgaben_nach_aufgabenbereichen_cofog
+WHERE aufgabenbereich_cofog_teilung = "Gesundheitswesen"
+    AND institutioneller_sektor = "Staat"
+    AND jahr <= 2020
+    AND jahr >= 2010
+GROUP BY jahr
+ORDER BY jahr ASC;
+
+-- Wie verteilen sich die kantonalen Bildungsausgaben im Jahr 2010?
+SELECT aufgabenbereich_cofog_gruppen, ausgaben_in_mio_chf as kantonale_ausgaben_in_mio_chf_jahr_2010
+FROM staatsausgaben_nach_aufgabenbereichen_cofog
+WHERE aufgabenbereich_cofog_teilung = "Bildungswesen"
+    AND jahr = 2020
+    AND institutioneller_sektor = "Kantone"
+ORDER BY ausgaben_in_mio_chf DESC;
