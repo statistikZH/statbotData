22c676c3d972ae8ecb229a116a6ca2f4824cc0cb
cmdoret <cyril.mattheydoret@gmail.com>
2023-08-31 19:04:53 +0200

    feat(A16): add example questions

---
+++ pipelines/A16/queries.sql
@@ -0,0 +1,21 @@
+-- How many residents were in Switzerland in 2018?
+
+-- What was the net migration of Switzerland from 2012 to 2022?
+
+-- What cantons had a negative population change in 2017?
+
+-- What was the canton with the most acquisition of Swiss citizensip in 2017 relative to its population?
+
+-- What canton has the most immigrants from other cantons?
+
+-- What are the 3 cantons with the highest relative population growth between 2000 and 2022?
+
+-- What was the natural population change in Canton Vaud between 2015 and 2020?
+
+-- Give me the total population of each canton in 2019.
+
+-- Show me the most populous canton every year between 1980 and 2000.
+
+-- What were the top 3 cantons with the lowest number of births relative to total population in 2022?
+
+-- What year saw the most births in Switzerland?
