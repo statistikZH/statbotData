e530e0bdb74c2c91321a1777b7150a21817e55c5
cmdoret <cyril.mattheydoret@gmail.com>
2023-09-11 14:21:10 +0200

    fix(A16): tweak questions + fix queries

--- pipelines/A16/queries.sql
+++ pipelines/A16/queries.sql
@@ -17,7 +17,7 @@ WHERE S.name="Switzerland"
     AND T.year<=2022
     AND T.citizenship_category="Citizenship (category) - total";
 
--- What cantons had a population decrease in 2017, and by how much?
+-- Which cantons experienced a population decline in 2017, and by how much?
 SELECT S.name, population_change
 FROM demographic_balance_by_canton as T
 JOIN spatial_unit as S on T.spatialunit_uid = S.spatialunit_uid
@@ -26,7 +26,7 @@ WHERE S.canton=TRUE
     AND T.year=2017
     AND T.citizenship_category="Citizenship (category) - total";
 
--- What were the 5 cantons with the most acquisition of Swiss citizensip in 2017 relative to their population?
+-- Which five cantons had the highest number of Swiss citizenship acquisitions in 2017 relative to their population?
 SELECT S.name, acquisition_of_swiss_citizenship / total_population as relative_acquisition
 FROM demographic_balance_by_canton as T
 JOIN spatial_unit as S on T.spatialunit_uid = S.spatialunit_uid
@@ -36,27 +36,35 @@ WHERE S.canton=TRUE
 ORDER BY relative_acquisition DESC
 LIMIT 5;
 
--- What canton has the most immigrants from other cantons?
-SELECT S.name, acquisition_of_swiss_citizenship / total_population as relative_acquisition
+-- Which canton has the highest number of residents who have moved from other cantons in 2016?
+SELECT S.name, T.in_migration_from_another_canton
 FROM demographic_balance_by_canton as T
 JOIN spatial_unit as S on T.spatialunit_uid = S.spatialunit_uid
 WHERE S.canton=TRUE
-    AND T.year=2017
-    AND T.citizenship_category="Switzerland"
-ORDER BY relative_acquisition DESC
-LIMIT 5;
+    AND T.year=2016
+    AND T.citizenship_category="Citizenship (category) - total"
+ORDER BY T.in_migration_from_another_canton DESC
+LIMIT 1;
 
--- What are the 5 cantons with the highest relative population growth between 2000 and 2022?
-SELECT S.name, acquisition_of_swiss_citizenship / total_population as relative_acquisition
+-- Which five cantons experienced the most significant population growth relative to their size between 2000 and 2022?
+SELECT
+    S.name, (
+        (
+            SUM(CASE WHEN T.year = 2022 THEN total_population ELSE 0 END) -
+            SUM(CASE WHEN T.year = 2000 THEN total_population ELSE 0 END)
+        ) / SUM(CASE WHEN T.year = 2000 THEN total_population ELSE 0 END)
+    ) as relative_population_growth
 FROM demographic_balance_by_canton as T
 JOIN spatial_unit as S on T.spatialunit_uid = S.spatialunit_uid
 WHERE S.canton=TRUE
-    AND T.year=2017
-    AND T.citizenship_category="Switzerland"
-ORDER BY relative_acquisition DESC
+    AND T.year>=2000
+    AND T.year<=2022
+    AND T.citizenship_category="Citizenship (category) - total"
+GROUP BY S.name
+ORDER BY relative_population_growth DESC
 LIMIT 5;
 
--- What was the natural population change of the Swiss residents in Canton Vaud between 2015 and 2020?
+-- What was the population change due to natural factors among the residents of Canton Vaud between 2015 and 2020?
 SELECT year, births - deaths
 FROM demographic_balance_by_canton as T
 JOIN spatial_unit as S on T.spatialunit_uid = S.spatialunit_uid
@@ -64,7 +72,7 @@ WHERE S.canton=TRUE
     AND S.name="Canton Vaud"
     AND T.year>=2015
     AND T.year>=2020
-    AND T.citizenship_category="Switzerland";
+    AND T.citizenship_category="Citizenship (category) - total";
 
 -- Give me the total population of each canton in 2019.
 SELECT S.name as canton, total_population as total_population_2019
@@ -75,7 +83,7 @@ WHERE S.canton=TRUE
     AND T.citizenship_category="Citizenship (category) - total"
 ORDER BY total_population DESC;
 
--- Show me the canton with the highest birthrate every year between 1995 and 2010.
+-- Show me the cantons with the highest birth rates every year between 1995 and 2010.
 SELECT year, S.name as most_populout_canton, MAX(births / total_population) as birthrate
 FROM demographic_balance_by_canton as T
 JOIN spatial_unit as S on T.spatialunit_uid = S.spatialunit_uid
@@ -86,7 +94,7 @@ WHERE S.canton=TRUE
 GROUP BY T.year
 ORDER BY year ASC;
 
--- What year saw the highest birt rate in Switzerland?
+-- In which year did Switzerland experience its highest birth rate?
 SELECT year, births / total_population as birthrate
 FROM demographic_balance_by_canton as T
 JOIN spatial_unit as S on T.spatialunit_uid = S.spatialunit_uid
