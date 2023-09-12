30c37e7bfac60f9c1710def2c1120b4a816f2a5a
cmdoret <cyril.mattheydoret@gmail.com>
2023-09-12 17:19:41 +0200

    feat(A22): add questions

---
+++ pipelines/A22/queries.sql
@@ -0,0 +1,23 @@
+-- How many evangelists are there in kanton Basel-Landschaft?
+
+-- What proportion of Basel-Landschaft's population are religious?
+
+-- Between foreigners and Swiss residents, who is more religious?
+
+-- What is the municipality with the highest proportion of protestants in Basel-Landschaft?
+
+-- What proportion of municipalities in Basel-Landschaft have more catholics than reformed?
+
+-- How did the proportion of christians change between 2006 and 2022 in Basel-Landschaft?
+
+-- How many municipalities are there in Basel-Landschaft where each religious confession is in majority?
+
+-- What are the top 5 municipalities with the highest proportion of foreign residents in canton Basel-Landschaft in 2022 ?
+
+-- How many swiss residents Binningen, BL are roman catholic?
+
+-- Among the foreign residents of Ettingen, in Canton Basel-Landschaft, what proportion are of unknown confession?
+
+-- Between the municipalities of Arlesheim and Birsfelden in Basel-Landschaft, which one has a higher proportion of protestants?
+
+-- How did the number of reformed evangelics evolve in Allschwil, in Canton Basel-Landschaft, between 2010 and 2015?
