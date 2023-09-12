c15eec878fe2248b893b21954e7e49d51bd3f7a1
cmdoret <cyril.mattheydoret@gmail.com>
2023-09-12 13:18:48 +0200

    feat(A22): add processing pipeline

---
+++ pipelines/A22/A22.R
@@ -0,0 +1,75 @@
+# -------------------------------------------------------------------------
+# Steps: Get the data
+# input: google sheet
+# output: ds$data, ds$dir
+# -------------------------------------------------------------------------
+
+ds <- statbotData::create_dataset("A22")
+ds <- statbotData::download_data(ds)
+
+# -------------------------------------------------------------------------
+# Step 1 rename columns
+# -------------------------------------------------------------------------
+
+ds$postgres_export <- ds$data %>%
+  janitor::clean_names() %>%
+  dplyr::select(jahr, gemeinde, nationalitaet, konfession, anzahl_personen)
+# -------------------------------------------------------------------------
+# Step 2 map to spatial units
+# -------------------------------------------------------------------------
+
+
+# Matching spatialunit uid to municipality/year/canton. Note that the same
+# municipality can have different uid at different years (e.g. in case of
+# merging/splitting municipalities).
+spatial_mapping <- ds$postgres_export %>%
+  dplyr::select(gemeinde, jahr) %>%
+  dplyr::distinct(gemeinde, jahr) %>%
+  dplyr::mutate(canton = "BL") %>%
+  statbotData::map_ds_municipalities(
+    year = jahr,
+    canton_abbr = canton,
+    municipality_name = gemeinde
+  ) %>%
+  dplyr::select(gemeinde, jahr, spatialunit_uid) %>%
+  dplyr::mutate(jahr = lubridate::year(jahr))
+
+
+# -------------------------------------------------------------------------
+# Step 3 clean names and remove columns
+# -------------------------------------------------------------------------
+
+# Note: we drop records without spatialunit_uid this happens for record
+# with an invalid municipality - date combination (according to spatialunits)
+# It only happens for ~40 records in 1990 in municipalities whose name were valid later
+# in spatialunits.
+ds$postgres_export <- ds$postgres_export %>%
+  dplyr::left_join(spatial_mapping, by = c("gemeinde", "jahr")) %>%
+  dplyr::filter(!is.na(spatialunit_uid)) %>%
+  dplyr::select(-gemeinde) %>%
+  janitor::clean_names()
+
+colnames(ds$postgres_export)
+
+# -------------------------------------------------------------------------
+# Step: Testrun queries on sqllite
+#   input:  ds$postgres_export, ds$dir/queries.sql
+#   output: ds$dir/queries.log
+# -------------------------------------------------------------------------
+
+statbotData::testrun_queries(
+  ds$postgres_export,
+  ds$dir,
+  ds$name
+)
+
+# -------------------------------------------------------------------------
+# Step: Write metadata tables
+#   input:  ds$postgres_export
+#   output: ds$dir/metadata_tables.csv
+#           ds$dir/metadata_table_columns.csv
+#           ds$dir/sample.csv
+# -------------------------------------------------------------------------
+
+read_write_metadata_tables(ds)
+dataset_sample(ds)
