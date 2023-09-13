0762d4f76222f542c550b9de3c77735a7f2f73f7
cmdoret <cyril.mattheydoret@gmail.com>
2023-09-13 17:08:42 +0200

    feat(A22): add initial queries

--- pipelines/A22/queries.sql
+++ pipelines/A22/queries.sql
@@ -1,12 +1,56 @@
--- How many evangelists are there in kanton Basel-Landschaft?
+-- How many protestants were there in kanton Basel-Landschaft in early 2022?
+SELECT SUM(T.anzahl_evangelisch_reformiert)
+FROM bevolkerungsbestand_nach_nationalitat_konfession_gemeinde_und_quartal_seit_2003 as T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal=TRUE
+    AND T.jahr="2022"
+    AND T.quartal="1";
 
--- What proportion of Basel-Landschaft's population are religious?
+-- What proportion of Basel-Landschaft's population were of a known religion in 2021?
+SELECT 1 - (
+    SUM(CAST(T.anzahl_unbekannt_konfession AS FLOAT)) / SUM(T.gesamt_anzahl_personen)
+) AS proportion_known_religion_basel_land_2021
+FROM bevolkerungsbestand_nach_nationalitat_konfession_gemeinde_und_quartal_seit_2003 as T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal=TRUE
+    AND T.jahr="2021"
+GROUP BY T.jahr;
 
--- Between foreigners and Swiss residents, who is more religious?
+-- In 2021, among the foreign residents of Canton Basel-Landschaft, what was the proportion of Roman catholics?
+SELECT SUM(CAST(T.anzahl_romisch_katholisch AS FLOAT)) / SUM(T.gesamt_anzahl_personen) AS proportion_foreign_romisch_katholisch_basel_land_2021
+FROM bevolkerungsbestand_nach_nationalitat_konfession_gemeinde_und_quartal_seit_2003 as T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal=TRUE
+    AND T.jahr="2021"
+    AND T.nationalitat="Ausland";
 
--- What is the municipality with the highest proportion of protestants in Basel-Landschaft?
+-- What were the 5 municipalities with the highest proportion of protestants in Basel-Landschaft in 2008?
+SELECT S.name, CAST(SUM(T.anzahl_evangelisch_reformiert) AS FLOAT) / SUM(T.gesamt_anzahl_personen) as proportion_protestants_2008
+FROM bevolkerungsbestand_nach_nationalitat_konfession_gemeinde_und_quartal_seit_2003 as T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal=TRUE
+    AND T.jahr="2008"
+GROUP BY S.name
+ORDER BY proportion_protestants_2008 DESC
+LIMIT 5;
 
--- What proportion of municipalities in Basel-Landschaft have more catholics than reformed?
+-- What was the proportion of municipalities in Basel-Landschaft where there were more catholics than reformed among Swiss residents in 2018?
+SELECT SUM(
+    (anzahl_romisch_katholisch + anzahl_christkatholisch) > anzahl_evangelisch_reformiert
+) / CAST(COUNT(anzahl_evangelisch_reformiert) AS FLOAT) AS proprtion_municipalities_more_catholics_than_reformed_2018
+FROM (
+    SELECT
+        S.name as municipality,
+        AVG(T.anzahl_christkatholisch) as anzahl_christkatholisch,
+        AVG(T.anzahl_romisch_katholisch) as anzahl_romisch_katholisch,
+        AVG(T.anzahl_evangelisch_reformiert) as anzahl_evangelisch_reformiert
+    FROM bevolkerungsbestand_nach_nationalitat_konfession_gemeinde_und_quartal_seit_2003 as T
+    JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+    WHERE S.municipal=TRUE
+        AND T.jahr="2018"
+        AND T.nationalitat="Schweiz"
+    GROUP BY S.name
+);
 
 -- How did the proportion of christians change between 2006 and 2022 in Basel-Landschaft?
 
