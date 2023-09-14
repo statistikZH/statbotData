ee1727e185a6926ae1a5e2ba46b3391c6128e892
cmdoret <cyril.mattheydoret@gmail.com>
2023-09-14 13:39:47 +0200

    feat(A22): add final queries and translate questions

--- pipelines/A22/queries.sql
+++ pipelines/A22/queries.sql
@@ -1,4 +1,4 @@
--- How many protestants were there in kanton Basel-Landschaft in early 2022?
+-- Wie viele Reformierte gab es im Kanton Basel-Landschaft Anfang 2022?
 SELECT SUM(T.anzahl_evangelisch_reformiert)
 FROM basel_land_bevolkerung_nach_nationalitat_konfession_gemeinde as T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -6,7 +6,7 @@ WHERE S.municipal=TRUE
     AND T.jahr=2022
     AND T.quartal=1;
 
--- What proportion of Basel-Landschaft's population were of a known religion in 2021?
+-- Welcher Anteil der Bevölkerung von Basel-Landschaft gehörte 2021 einer bekannten Religion an?
 SELECT 1 - (
     SUM(CAST(T.anzahl_unbekannt_konfession AS FLOAT)) / SUM(T.gesamt_anzahl_personen)
 ) AS proportion_known_religion_basel_land_2021
@@ -16,7 +16,7 @@ WHERE S.municipal=TRUE
     AND T.jahr=2021
 GROUP BY T.jahr;
 
--- In 2021, among the foreign residents of Canton Basel-Landschaft, what was the proportion of Roman catholics?
+-- Wie hoch war der Anteil der römisch-katholischen Personen an der ausländischen Bevölkerung des Kantons Basel-Landschaft im Jahr 2021?
 SELECT SUM(CAST(T.anzahl_romisch_katholisch AS FLOAT)) / SUM(T.gesamt_anzahl_personen) AS proportion_foreign_romisch_katholisch_basel_land_2021
 FROM basel_land_bevolkerung_nach_nationalitat_konfession_gemeinde as T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -24,7 +24,7 @@ WHERE S.municipal=TRUE
     AND T.jahr=2021
     AND T.nationalitat="Ausland";
 
--- What were the 5 municipalities with the highest proportion of protestants in Basel-Landschaft in 2008?
+-- Welche 5 Gemeinden hatten 2008 den höchsten Anteil an Reformierten im Kanton Basel-Landschaft?
 SELECT S.name, CAST(SUM(T.anzahl_evangelisch_reformiert) AS FLOAT) / SUM(T.gesamt_anzahl_personen) as proportion_protestants_2008
 FROM basel_land_bevolkerung_nach_nationalitat_konfession_gemeinde as T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -34,7 +34,7 @@ GROUP BY S.name
 ORDER BY proportion_protestants_2008 DESC
 LIMIT 5;
 
--- What was the proportion of municipalities in Basel-Landschaft where there were more catholics than protestants among Swiss residents in 2018?
+-- Wie hoch war der Anteil der Gemeinden im Kanton Basel-Landschaft, in denen es 2018 mehr Katholiken als Protestanten unter der Schweizer Bevölkerung gab?
 SELECT SUM(
     (anzahl_romisch_katholisch + anzahl_christkatholisch) > anzahl_evangelisch_reformiert
 ) / CAST(COUNT(anzahl_evangelisch_reformiert) AS FLOAT) AS proprtion_municipalities_more_catholics_than_reformed_2018
@@ -52,7 +52,7 @@ FROM (
     GROUP BY S.name
 );
 
--- Show me the evolution of the population of Pratteln, in Basel-Landschaft, between 2017 and 2019.
+-- Zeigen Sie mir die Entwicklung der Bevölkerung von Pratteln, in Basel-Landschaft, zwischen 2017 und 2019.
 SELECT T.jahr, T.quartal, SUM(T.gesamt_anzahl_personen) as population
 FROM basel_land_bevolkerung_nach_nationalitat_konfession_gemeinde as T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -64,7 +64,7 @@ GROUP BY T.jahr, T.quartal
 ORDER BY T.jahr, T.quartal ASC;
 
 
--- What were the 3 municipalities with the highest proportion of foreign residents in canton Basel-Landschaft in late 2022 ?
+-- Welches waren Ende 2022 die 3 Gemeinden mit dem höchsten Ausländeranteil im Kanton Basel-Landschaft?
 SELECT S.name, SUM(CASE WHEN T.nationalitat="Ausland" THEN T.gesamt_anzahl_personen ELSE 0 END) / CAST(SUM(T.gesamt_anzahl_personen) AS FLOAT) AS proportion_foreign_residents
 FROM basel_land_bevolkerung_nach_nationalitat_konfession_gemeinde as T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -75,7 +75,7 @@ GROUP BY S.name
 ORDER BY proportion_foreign_residents DESC
 LIMIT 3;
 
--- How many swiss residents Binningen, BL were roman catholic during the year 2005?
+-- Wie viele Schweizer Einwohner hatte Binningen, BL im Jahr 2005?
 SELECT T.jahr, T.quartal, T.anzahl_romisch_katholisch
 FROM basel_land_bevolkerung_nach_nationalitat_konfession_gemeinde as T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -84,8 +84,31 @@ WHERE S.municipal=TRUE
     AND T.jahr=2005
     AND T.nationalitat="Schweiz";
 
--- Among the foreign residents of Ettingen, in Canton Basel-Landschaft, what proportion are of unknown confession?
+-- Welcher Anteil der ausländischen Bevölkerung von Ettingen, BL, hatte 2011 eine unbekannte Konfession?
+SELECT SUM(CAST(T.anzahl_unbekannt_konfession AS FLOAT)) / SUM(T.gesamt_anzahl_personen) AS proportion_unknown_religion_ettingen_2011
+FROM basel_land_bevolkerung_nach_nationalitat_konfession_gemeinde as T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal=TRUE
+    AND S.name="Ettingen"
+    AND T.nationalitat="Ausland"
+    AND T.jahr=2011;
 
--- Between the municipalities of Arlesheim and Birsfelden in Basel-Landschaft, which one has a higher proportion of protestants?
+-- Zwischen den Gemeinden Arlesheim und Birsfelden im Kanton Basel-Landschaft, welche hatte 2003 einen höheren Anteil an Protestanten?
+SELECT S.name, CAST(SUM(T.anzahl_evangelisch_reformiert) AS FLOAT) / SUM(T.gesamt_anzahl_personen) as proportion_protestants_2003
+FROM basel_land_bevolkerung_nach_nationalitat_konfession_gemeinde as T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal=TRUE
+    AND S.name IN ("Ettingen", "Birsfelden")
+    AND T.jahr=2003
+GROUP BY S.name
+ORDER BY proportion_protestants_2003 DESC;
 
--- How did the number of reformed evangelics evolve in Allschwil, in Canton Basel-Landschaft, between 2010 and 2015?
+-- Wie hat sich die Anzahl der Reformierten in Allschwil, im Kanton Basel-Landschaft, zwischen 2010 und 2015 entwickelt?
+SELECT T.jahr, T.quartal, SUM(T.anzahl_evangelisch_reformiert) as number_reformed_evangelics
+FROM basel_land_bevolkerung_nach_nationalitat_konfession_gemeinde as T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal=TRUE
+    AND S.name="Allschwil"
+    AND T.jahr>=2010
+    AND T.jahr<=2015
+GROUP BY T.jahr, T.quartal;
