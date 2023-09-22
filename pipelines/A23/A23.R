be5e165ea4c2014ae599215fdb60fe7823aa6c36
Cyril Matthey-Doret <cmdoret@mailbox.org>
2023-09-22 16:50:22 +0000

    pipeline + queriesDs a23 (#82)
    * fix(meta): crash due to auto collapsing on df when n_examples ==1
    
    * feat(A22): add processing pipeline
    
    * doc(A22): add sample
    
    * doc(A22): add metadata
    
    * doc(A21): add missing spatial resolution in metadata
    
    * feat(A22): add questions
    
    * fix(A22): widen on konfession
    
    * fix(A22): explicitly add 0s when pivoting
    
    * feat(A22): add initial queries
    
    * fix(A22): shorter table name, more queries
    
    * feat(A22): add final queries and translate questions
    
    * feat(A23): add pipeline
    
    * doc(A23): add metadata
    
    * fix(A23): explicitely cast falle, preis and flache to int
    
    * feat(A23): add question/query pairs
    
    * feat(A23): additional queries

---
+++ pipelines/A23/A23.R
@@ -0,0 +1,73 @@
+# -------------------------------------------------------------------------
+# Steps: Get the data
+# input: google sheet
+# output: ds$data, ds$dir
+# -------------------------------------------------------------------------
+
+ds <- statbotData::create_dataset("A23")
+ds <- statbotData::download_data(ds)
+
+# -------------------------------------------------------------------------
+# Step 1 rename columns + clean values
+# -------------------------------------------------------------------------
+
+# The dataset has missing values represented as "" or "( )"
+ds$postgres_export <- ds$data %>%
+  janitor::clean_names() %>%
+  dplyr::filter(!falle %in% c("", "( )")) %>%
+  dplyr::select(jahr, gemeinde, falle, flache_in_m2, quadratmeterpreis_chf) %>%
+  dplyr::mutate_at(dplyr::vars(flache_in_m2, quadratmeterpreis_chf, falle), as.numeric)
+# -------------------------------------------------------------------------
+# Step 2 map to spatial units
+# -------------------------------------------------------------------------
+
+# Matching spatialunit uid to municipality/year/canton. Note that the same
+# municipality can have different uid at different years (e.g. in case of
+# merging/splitting municipalities).
+spatial_mapping <- ds$postgres_export %>%
+  dplyr::select(gemeinde, jahr) %>%
+  dplyr::distinct(gemeinde, jahr) %>%
+  dplyr::mutate(canton = "BL") %>%
+  statbotData::map_ds_municipalities(
+    year = jahr,
+    canton_abbr = canton,
+    municipality_name = gemeinde
+  ) %>%
+  dplyr::select(gemeinde, jahr, spatialunit_uid) %>%
+  dplyr::mutate(jahr = lubridate::year(jahr))
+
+
+# -------------------------------------------------------------------------
+# Step 3 clean names and remove columns
+# -------------------------------------------------------------------------
+
+ds$postgres_export <- ds$postgres_export %>%
+  dplyr::left_join(spatial_mapping, by = c("gemeinde", "jahr")) %>%
+  dplyr::filter(!is.na(spatialunit_uid)) %>%
+  dplyr::select(-gemeinde) %>%
+  janitor::clean_names()
+
+colnames(ds$postgres_export)
+
+# -------------------------------------------------------------------------
+# Step: Testrun queries on sqllite
+#   input:  ds$postgres_export, ds$dir/queries.sql
+#   output: ds$dir/queries.log
+# -------------------------------------------------------------------------
+
+statbotData::testrun_queries(
+  ds$postgres_export,
+  ds$dir,
+  ds$name
+)
+
+# -------------------------------------------------------------------------
+# Step: Write metadata tables
+#   input:  ds$postgres_export
+#   output: ds$dir/metadata_tables.csv
+#           ds$dir/metadata_table_columns.csv
+#           ds$dir/sample.csv
+# -------------------------------------------------------------------------
+
+read_write_metadata_tables(ds)
+dataset_sample(ds)
