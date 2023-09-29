2396c8a00357ea20a41aa179f1ca61d5cf54ed15
cmdoret <cyril.mattheydoret@gmail.com>
2023-09-29 17:10:57 +0200

    feat(A24): add 10 questions

---
+++ pipelines/A24/queries.sql
@@ -0,0 +1,19 @@
+-- What municipality in the canton Aargau had the most cars per inhabitant?
+
+-- How many agricultural vehicles were there in the canton Aargau in 2010?
+
+-- How did the number of commercial vehicles change between 1960 and 1980 in the canton Aargau?
+
+-- How did the number of motorcycles in canton Aargau and personal vehicles evolve between 1930 and 1960?
+
+-- What is the proportion of municipalities in canton Aargau where the number of cars per inhabitants increased between 2015 and 2022?
+
+-- What was the number of trailers from 1998 to 2001 in Aarburg, AG?
+
+-- What were the top 3 municipalities in Aargau with the highest proportion of motorcycles vs cars in 2015?
+
+-- What was the number of commercial vehicles in Abtwil, AG in 2000 ?
+
+-- How did the number of cars per inhabitant in Aargau evolve over the period between 2010 and 2022?
+
+-- What municipalities in Aargau have the fewest motorcycle per passenger car?
