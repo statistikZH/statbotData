902b2684f7ff20173f795e6fc3c00152419dcb7a
Cyril Matthey-Doret <cmdoret@mailbox.org>
2023-10-02 14:56:24 +0000

    pipeline+queries a24 (#87)
    * feat(download_data): Support for json download
    
    * feat(A24): add dataset to input list
    
    * feat(A24): add pipeline
    
    * feat(A24): add metadata
    
    * feat(A24): add sample
    
    * feat(A24): add 10 questions
    
    * feat(A24): Add initial 10 question/query pairs
    
    * feat(A24): add 20 question query pairs
    
    * feat(A24): translate questions to de

---
+++ pipelines/A24/A24.R
@@ -0,0 +1,95 @@
+# -------------------------------------------------------------------------
+# Steps: Get the data
+# input: google sheet
+# output: ds$data, ds$dir
+# -------------------------------------------------------------------------
+
+ds <- statbotData::create_dataset("A24")
+ds <- statbotData::download_data(ds)
+
+# -------------------------------------------------------------------------
+# Step 1 rename columns + clean values
+# -------------------------------------------------------------------------
+
+# The dataset has missing values represented as "" or "( )"
+ds$postgres_export <- ds$data %>%
+  janitor::clean_names() %>%
+  dplyr::filter(location_type %in% c("TOWNSHIP", "CANTON")) %>%
+  dplyr::select(
+    location_name, location_type, year, pers_wagen, cars_klbusse, nutzfz, arb_motorfz, motorr, kl_motorr, motorfahrr, landw_motorfz, anhaenger, kollek_fz, mot_grad
+  )
+# -------------------------------------------------------------------------
+# Step 2 map to spatial units
+# -------------------------------------------------------------------------
+
+# Matching spatialunit uid to municipality/year/canton. Note that the same
+# municipality can have different uid at different years (e.g. in case of
+# merging/splitting municipalities).
+municipality_mapping <- ds$postgres_export %>%
+  dplyr::filter(location_type == "TOWNSHIP") %>%
+  dplyr::select(location_name, year) %>%
+  dplyr::distinct(location_name, year) %>%
+  dplyr::mutate(canton = "AG") %>%
+  statbotData::map_ds_municipalities(
+    year = year,
+    canton_abbr = canton,
+    municipality_name = location_name
+  ) %>%
+  dplyr::select(location_name, year, spatialunit_uid) %>%
+  dplyr::mutate(year = lubridate::year(year))
+
+cantonal_data <- ds$postgres_export %>%
+  dplyr::filter(location_type == "CANTON") %>%
+  dplyr::mutate(spatialunit_uid = "19_A.ADM1")
+
+ds$postgres_export <- ds$postgres_export %>%
+  dplyr::filter(location_type == "TOWNSHIP") %>%
+  dplyr::left_join(municipality_mapping, by = c("location_name", "year")) %>%
+  dplyr::bind_rows(cantonal_data) %>%
+  dplyr::filter(!is.na(spatialunit_uid)) %>%
+  dplyr::select(-location_name, -location_type) %>%
+  janitor::clean_names()
+
+# -------------------------------------------------------------------------
+# Step 3 clean names and remove columns
+# -------------------------------------------------------------------------
+
+ds$postgres_export <- ds$postgres_export %>%
+  dplyr::rename(
+    jahr = year,
+    anzahl_personenwagen = pers_wagen,
+    anzahl_ubrige_personen_transportfahrzeuge_kleinbusse_cars = cars_klbusse,
+    anzahl_nutzfahrzeuge = nutzfz,
+    anzahl_arbeitsmotorfahrzeuge = arb_motorfz,
+    anzahl_motorrader = motorr,
+    anzahl_klein_motorrader = kl_motorr,
+    anzahl_motorfahrrader = motorfahrr,
+    anzahl_landwirtschaftliche_motorfahrzeuge = landw_motorfz,
+    anzahl_anhaenger = anhaenger,
+    anzahl_kollektivfahrzeuge = kollek_fz,
+    anzahl_personenwagen_pro_1000_einwohner = mot_grad
+  )
+colnames(ds$postgres_export)
+
+# -------------------------------------------------------------------------
+# Step: Testrun queries on sqllite
+#   input:  ds$postgres_export, ds$dir/queries.sql
+#   output: ds$dir/queries.log
+# -------------------------------------------------------------------------
+
+statbotData::testrun_queries(
+  ds$postgres_export,
+  ds$dir,
+  ds$name
+)
+
+# -------------------------------------------------------------------------
+# Step: Write metadata tables
+#   input:  ds$postgres_export
+#   output: ds$dir/metadata_tables.csv
+#           ds$dir/metadata_table_columns.csv
+#           ds$dir/sample.csv
+# -------------------------------------------------------------------------
+
+read_write_metadata_tables(ds)
+dataset_sample(ds)
---
+++ pipelines/A24/queries.sql
@@ -0,0 +1,196 @@
+-- Welche Gemeinde im Kanton Aargau hatte im Jahr 2020 am meisten Autos pro Einwohner?
+SELECT S.name
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal=TRUE
+    AND T.jahr=2020
+ORDER BY T.anzahl_personenwagen_pro_1000_einwohner DESC
+LIMIT 1;
+
+-- Wie viele landwirtschaftliche Fahrzeuge gab es im Kanton Aargau im Jahr 2010?
+SELECT anzahl_landwirtschaftliche_motorfahrzeuge
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.canton=TRUE
+    AND S.name = "Canton Aargau"
+    AND T.jahr=2010;
+
+-- Wie hoch war die Zahl der Nutzfahrzeuge 1960 und 1980 im Kanton Aargau?
+SELECT T.jahr, anzahl_nutzfahrzeuge
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.canton=TRUE
+    AND S.name = "Canton Aargau"
+    AND T.jahr IN (1960, 1980);
+
+-- Wie hat sich der Bestand an Motorrädern und Personenwagen im Kanton Aargau im Zeitraum zwischen 1930 und 1960 entwickelt?
+SELECT T.jahr, anzahl_motorrader, anzahl_klein_motorrader, anzahl_motorfahrrader, anzahl_personenwagen
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.canton=TRUE
+    AND S.name = "Canton Aargau"
+    AND T.jahr >= 1930
+    AND T.jahr <= 1960;
+
+-- Wie hoch ist der Anteil der Gemeinden im Kanton Aargau, in denen die Zahl der Autos pro Einwohner zwischen 2015 und 2022 zugenommen hat?
+SELECT CAST(SUM(CASE WHEN T1.diff > 0 THEN 1 ELSE 0 END) AS FLOAT) / COUNT(T1.diff)
+FROM (
+    SELECT
+        SUM(CASE WHEN T.jahr = 2022 THEN T.anzahl_personenwagen_pro_1000_einwohner ELSE 0 END) -
+        SUM(CASE WHEN T.jahr = 2015 THEN T.anzahl_personenwagen_pro_1000_einwohner ELSE 0 END) as diff
+    FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+    JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+    WHERE S.municipal=TRUE
+        AND T.jahr IN (2015, 2022)
+    GROUP BY S.name
+) as T1;
+
+-- Wie hoch war die Zahl der Wohnwagen von 1998 bis 2001 in Aarburg, AG?
+SELECT T.jahr, T.anzahl_anhaenger
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal=TRUE
+    AND S.name = "Aarburg"
+    AND T.jahr >= 1998
+    AND T.jahr <= 2001;
+
+-- Welches waren 2015 die Top-3-Gemeinden im Aargau mit dem höchsten Anteil an Motorrädern gegenüber Autos? Zeigen Sie auch die Proportionen.
+SELECT
+    S.name,
+    CAST(T.anzahl_motorrader + T.anzahl_klein_motorrader + T.anzahl_motorfahrrader AS FLOAT) / T.anzahl_personenwagen AS ratio_motorrader_to_personenwagen
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal=TRUE
+    AND T.jahr=2015
+ORDER BY ratio_motorrader_to_personenwagen DESC
+LIMIT 3;
+
+-- Wie hoch war die Zahl der Nutzfahrzeuge in Abtwil, AG im Jahr 2000 ?
+SELECT T.anzahl_nutzfahrzeuge
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal=TRUE
+    AND S.name = "Abtwil"
+    AND T.jahr=2000;
+
+-- Wie hat sich die Zahl der Autos pro Einwohner im Aargau im Zeitraum 2010 bis 2022 entwickelt?
+SELECT T.jahr, T.anzahl_personenwagen_pro_1000_einwohner
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.canton=TRUE
+    AND S.name = "Canton Aargau"
+    AND T.jahr >= 2010
+    AND T.jahr <= 2022;
+
+-- Welches waren die 5 Aargauer Gemeinden mit den wenigsten Motorrädern pro Personenwagen im Jahr 2015?
+SELECT S.name
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE T.jahr=2015
+    AND S.municipal=TRUE
+ORDER BY CAST(T.anzahl_motorrader + T.anzahl_klein_motorrader + T.anzahl_motorfahrrader AS FLOAT) / T.anzahl_personenwagen ASC
+LIMIT 5;
+
+-- Wie viele private Fahrzeuge gab es 2010 in Ueken im Kanton Aargau von jedem Typ?
+SELECT
+    T.anzahl_anhaenger,
+    T.anzahl_arbeitsmotorfahrzeuge,
+    T.anzahl_klein_motorrader,
+    T.anzahl_kollektivfahrzeuge,
+    T.anzahl_landwirtschaftliche_motorfahrzeuge,
+    T.anzahl_motorfahrrader,
+    T.anzahl_motorrader,
+    T.anzahl_nutzfahrzeuge,
+    T.anzahl_personenwagen,
+    T.anzahl_ubrige_personen_transportfahrzeuge_kleinbusse_cars
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE T.jahr = 2010
+    AND S.municipal = TRUE
+    AND S.name = "Ueken";
+
+-- In welchem Jahr gab es in Zuzgen, AG, die meisten motorisierten Arbeitsfahrzeuge?
+SELECT T.jahr
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal = TRUE
+    AND S.name = "Zuzgen"
+GROUP BY T.jahr
+ORDER BY T.anzahl_arbeitsmotorfahrzeuge DESC
+LIMIT 1;
+
+-- Gab es 1930 oder 1940 im Kanton Aargau mehr Personenwagen?
+SELECT T.jahr
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.canton = TRUE
+    AND T.jahr IN (1930, 1940)
+GROUP BY T.jahr
+ORDER BY T.anzahl_personenwagen DESC
+LIMIT 1;
+
+-- Zeigen Sie mir 3 Aargauer Gemeinden, in denen die Zahl der Personenwagen pro Einwohner zwischen 2018 und 2022 am stärksten abgenommen hat.
+SELECT S.name
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal = TRUE
+    AND T.jahr IN (2018, 2022)
+GROUP BY S.name
+ORDER BY
+    (
+        SUM(CASE WHEN T.jahr = 2022 THEN T.anzahl_personenwagen_pro_1000_einwohner ELSE 0 END) -
+        SUM(CASE WHEN T.jahr = 2018 THEN T.anzahl_personenwagen_pro_1000_einwohner ELSE 0 END)
+    ) DESC
+LIMIT 3;
+
+-- Wie viele Aargauer Gemeinden haben im Jahr 2020 keine Personenwagen gezählt?
+SELECT COUNT(*)
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten
+WHERE jahr = 2020
+    AND anzahl_personenwagen = 0;
+
+-- Wie viele Kleinmotorräder gab es im Kanton Aargau zwischen 2000 und 2016?
+SELECT T.jahr, T.anzahl_klein_motorrader
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit as S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.canton = TRUE
+    AND T.jahr <= 2016
+    AND T.jahr >= 2000;
+
+
+-- Wo im Aargau hatten Zeihen und Zufikon im Jahr 2001 mehr Personenwagen pro Einwohner?
+SELECT S.name
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit as S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal = TRUE
+    AND T.jahr = 2001
+    AND S.name IN ("Zeihen", "Zufikon")
+ORDER BY T.anzahl_personenwagen_pro_1000_einwohner DESC
+LIMIT 1;
+
+
+-- Wie viele landwirtschaftliche Fahrzeuge gab es in Rekingen im Aargau zwischen 1998 und 2000?
+SELECT T.jahr, T.anzahl_landwirtschaftliche_motorfahrzeuge
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.name LIKE "Rekingen%"
+    AND T.jahr >= 1998
+    AND T.jahr <= 2000
+ORDER BY T.jahr ASC;
+
+
+-- Wie viele Kollektivfahrzeuge gab es 2022 in Wettingen, AG?
+SELECT T.anzahl_kollektivfahrzeuge
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal = TRUE
+    AND S.name = "Wettingen"
+    AND T.jahr = 2022;
+
+-- Wie viele Anhänger gab es im Jahr 2000 in Suhr, Aargau?
+SELECT T.anzahl_anhaenger
+FROM aargau_privatverkehr_bestand_nach_fahrzeugarten AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal = TRUE
+    AND S.name = "Suhr"
+    AND T.jahr = 2000;
