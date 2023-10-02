d87811d0184e19ff5756abfb4d713d0c04482fdb
cmdoret <cyril.mattheydoret@gmail.com>
2023-10-02 16:37:49 +0200

    feat(A25): add 12 questions

---
+++ pipelines/A25/queries.sql
@@ -0,0 +1,23 @@
+-- What area was dedicated to growing red wine in Aargau in 2017?
+
+-- How much wine overall was harvested in Aargau in 1980 and 2010?
+
+-- By how much did the total value of wine harvests in Aargau change between 1980 and 2017?
+
+-- How much red and white wine was harvested in Aargau in 2019?
+
+-- What proportion of red wine harvested in Aargau was from crossings in 2000?
+
+-- In what year was the most wine harvested in Aargau?
+
+-- What was the value of white wine produced in Aargau each year between 2003 and 2008?
+
+-- What was the relative change in area for wine production in Aargau between 1980 and 2000?
+
+-- What was the proportion of the value of wine produced in Aargau that was from crossings in 2017?
+
+-- How many square kilometers of vineyards were in Aargau in 2018?
+
+-- How many liters of wines were produced in Aargau in total between 2000 and 2010?
+
+-- What was the difference in CHF between the value of red european wine produced in Aargau in 1999 and 2000?
