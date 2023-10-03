d195e3448d8a3fa8568a041c92ecb8b6ce306d27
Sabine Maennel <5292683+sabinem@users.noreply.github.com>
2023-10-03 12:00:54 +0200

    A17: done, queries don't run

--- pipelines/A17/A17.R
+++ pipelines/A17/A17.R
@@ -1,7 +1,7 @@
 # -------------------------------------------------------------------------
-# Steps: Get the data
-# input: google sheet
-# output: ds$data, ds$dir
+# Step: Get the data
+# input: data/const/statbot_input_data.csv
+# output: ds as dataset class
 # -------------------------------------------------------------------------
 
 ds <- statbotData::create_dataset("A17")
@@ -59,26 +59,22 @@ spatial_map <- ds$cleaned_data %>%
 ds$postgres_export <- ds$cleaned_data %>%
   dplyr::left_join(spatial_map, by = "kanton") %>%
   dplyr::select(-kanton)
+ds$postgres_export
 
 # -------------------------------------------------------------------------
-# Step: Testrun queries on sqllite
-#   input:  ds$postgres_export, ds$dir/queries.sql
-#   output: ds$dir/queries.log
+# Step: After the dataset has been build use functions of package stabotData
+# to upload the dataset to postgres, testrun the queries, generate a sample
+# upload the metadata, etc
 # -------------------------------------------------------------------------
 
-statbotData::testrun_queries(
-  ds$postgres_export,
-  ds$dir,
-  ds$name
-)
+# testrun queries
+statbotData::testrun_queries(ds)
 
-# -------------------------------------------------------------------------
-# Step: Write metadata tables
-#   input:  ds$postgres_export
-#   output: ds$dir/metadata_tables.csv
-#           ds$dir/metadata_table_columns.csv
-#           ds$dir/sample.csv
-# -------------------------------------------------------------------------
+# create the table in postgres
+statbotData::create_postgres_table(ds)
 
-read_write_metadata_tables(ds)
-dataset_sample(ds)
+# add the metadata to postgres
+statbotData::update_metadata_in_postgres(ds)
+
+# generate sample data for the dataset from the local tibble
+statbotData::dataset_sample(ds)
