838d029f282e195165ad254e756c53cf3b5e1711
cmdoret <cyril.mattheydoret@gmail.com>
2023-10-03 19:37:21 +0200

    feat(A26): translate questions to de

--- pipelines/A26/queries.sql
+++ pipelines/A26/queries.sql
@@ -1,4 +1,4 @@
--- How much insurance money was claimed in total for fire-related incidents in Aargau in 2017 in CHF?
+-- Wie hoch waren die Versicherungssummen für Brandschäden im Aargau im Jahr 2017 insgesamt in CHF?
 SELECT T.total_schaden_1000_chf * 1000 AS total_schaden_chf
 FROM aargau_brandversicherung_brandschaden_anzahl_schadensummen AS T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -6,7 +6,7 @@ WHERE S.canton = TRUE
     AND S.name LIKE '%Aargau%'
     AND T.jahr = 2017;
 
--- How many fire-related incidents were there in Aargau in 2011?
+-- Wie viele Brandfälle gab es im Jahr 2011 im Aargau?
 SELECT T.total_anzahl_schadenfalle
 FROM aargau_brandversicherung_brandschaden_anzahl_schadensummen AS T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -14,7 +14,7 @@ WHERE S.canton = TRUE
     AND S.name LIKE '%Aargau%'
     AND T.jahr = 2011;
 
--- How many explosions and arsons were recorded in Aargau in 2000?
+-- Wie viele Explosionen und Brandstiftungen wurden im Jahr 2000 im Aargau registriert?
 SELECT T.explosion_anzahl_schadenfalle, T.vorsatzliche_brandstift_anzahl_schadenfalle
 FROM aargau_brandversicherung_brandschaden_anzahl_schadensummen AS T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -22,7 +22,7 @@ WHERE S.canton = TRUE
     AND S.name LIKE '%Aargau%'
     AND T.jahr = 2000;
 
--- In what year were the most arsons reported in Aargau?
+-- In welchem Jahr wurden im Aargau die meisten Brandstiftungen gemeldet?
 SELECT T.jahr
 FROM aargau_brandversicherung_brandschaden_anzahl_schadensummen AS T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -31,7 +31,7 @@ WHERE S.canton = TRUE
 ORDER BY T.vorsatzliche_brandstift_anzahl_schadenfalle DESC
 LIMIT 1;
 
--- What were the 3 most frequent causes of fire-related incidents reported in Aargau in 1985?
+-- Welches waren die 3 häufigsten Ursachen für Brandfälle im Aargau im Jahr 1985?
 WITH T1 AS (
     SELECT *
     FROM aargau_brandversicherung_brandschaden_anzahl_schadensummen AS T
@@ -66,7 +66,7 @@ FROM (
 ORDER BY T2.source DESC
 LIMIT 3;
 
--- What cause of fire-related incidents cost the most money to insurances in Aargau in 2020?
+-- Welche Brandursache kostet die Aargauer Versicherungen im Jahr 2020 am meisten Geld?
 WITH T1 AS (
     SELECT *
     FROM aargau_brandversicherung_brandschaden_anzahl_schadensummen AS T
@@ -101,7 +101,7 @@ FROM (
 ORDER BY T2.source DESC
 LIMIT 1;
 
--- In percent, how did the number of electrical fires change from 1990 to 2020 in canton Aargau?
+-- In Prozent, wie hat sich die Zahl der Elektrobrände im Kanton Aargau von 1990 bis 2020 verändert?
 SELECT 100.0 * (T1.anzahl_2020 - T1.anzahl_1990) / T1.anzahl_1990 AS prozent_anderung_elektrizitaet_anzahl_schadenfalle_1990_2020
 FROM (
     SELECT
@@ -113,7 +113,8 @@ FROM (
         AND S.name LIKE '%Aargau%'
         AND T.jahr IN (1990, 2020)
 ) AS T1;
--- How much did fire incidents caused by explosions cost in CHF to insurances in canton Aargau in years 1980, 1990, 2000 and 2010?
+
+-- Wie viel kosteten Brandfälle durch Explosionen die Versicherer im Kanton Aargau in den Jahren 1980, 1990, 2000 und 2010 in CHF?
 SELECT T.jahr, T.explosion_schaden_1000_chf * 1000 AS explosion_schaden_chf
 FROM aargau_brandversicherung_brandschaden_anzahl_schadensummen AS T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -122,7 +123,7 @@ WHERE S.canton = TRUE
     AND T.jahr IN (1980, 1990, 2000, 2010)
 ORDER BY T.jahr DESC;
 
--- What was the cost in CHF of fire-damage caused by electricity and explosions in canton Aargau every year between 2001 and 2007?
+-- Wie hoch waren die jährlichen Kosten in CHF für die durch Elektrizität und Explosionen verursachten Brandschäden im Kanton Aargau in den Jahren 2001 bis 2007?
 SELECT T.jahr, T.elektrizitaet_schaden_1000_chf * 1000 AS elektrizitaet_schaden_chf
 FROM aargau_brandversicherung_brandschaden_anzahl_schadensummen AS T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -132,7 +133,7 @@ WHERE S.canton = TRUE
     AND T.jahr <= 2007
 ORDER BY T.jahr DESC;
 
--- How much CHF did fire incidents from unknown sources cost in Aargau in 2020?
+-- Wie viel Franken kosteten Brandeinsätze aus unbekannter Ursache im Aargau im Jahr 2020?
 SELECT T.unbekannt_schaden_1000_chf * 1000 AS unbekannt_schaden_chf
 FROM aargau_brandversicherung_brandschaden_anzahl_schadensummen AS T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -140,7 +141,7 @@ WHERE S.canton = TRUE
     AND S.name LIKE '%Aargau%'
     AND T.jahr = 2020;
 
--- What percentage of the number of fire-related incidents came from unknown causes in Aargau in 2018?
+-- Wie hoch (in prozent) ist der Anteil unbekannter Ursachen an der Zahl der Brandfälle im Aargau im Jahr 2018?
 SELECT T.unbekannt_anzahl_schadenfalle * 100.0 / T.total_anzahl_schadenfalle AS unbekannt_anzahl_schadenfalle_prozent
 FROM aargau_brandversicherung_brandschaden_anzahl_schadensummen AS T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
@@ -148,7 +149,7 @@ WHERE S.canton = TRUE
     AND S.name LIKE '%Aargau%'
     AND T.jahr = 2018;
 
--- How many arsons were reported in total from 1980 to 2020 in canton Aargau?
+-- Wie viele Brandstiftungen wurden von 1980 bis 2020 im Kanton Aargau insgesamt gemeldet?
 SELECT SUM(T.vorsatzliche_brandstift_anzahl_schadenfalle)
 FROM aargau_brandversicherung_brandschaden_anzahl_schadensummen AS T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
