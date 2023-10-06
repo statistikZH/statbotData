0c3e69c23ebad1c1869af15a7fc78670e13e4970
cmdoret <cyril.mattheydoret@gmail.com>
2023-10-06 18:22:38 +0200

    feat(A28): add 6 queries

--- pipelines/A28/queries.sql
+++ pipelines/A28/queries.sql
@@ -2,16 +2,66 @@
 SELECT T.beschaftigte_personen
 FROM thurgau_beschaftigte_nach_sektoren_und_gemeinden AS T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
-WHERE S.municipal=TRUE
+WHERE S.municipal = TRUE
     AND S.name = "Uttwil"
-    AND T.jahr=2015
+    AND T.jahr = 2015
     AND T.sektor = 1;
+
 -- How many employees of the tertiary sector were employed in canton Thurgau in 2011 and 2021?
--- What are the five municipalities in Thurgau with the highest proportion of employees in the secondary sector in 2020?
+SELECT T.jahr, SUM(T.beschaftigte_personen)
+FROM thurgau_beschaftigte_nach_sektoren_und_gemeinden AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal=TRUE
+    AND T.jahr IN (2011, 2021)
+    AND T.sektor = 3
+GROUP BY T.jahr;
+
+-- What are the five municipalities in Thurgau with the highest proportion of employees in the secondary sector in 2020? Also report the proportions in percentage.
+SELECT
+    S.name,
+    (
+        100.0 * SUM(CASE WHEN T.sektor = 2 THEN T.beschaftigte_personen ELSE 0 END) /
+        SUM(T.beschaftigte_personen)
+    ) as prozent_beschaftigte_personen_sektor_2
+FROM thurgau_beschaftigte_nach_sektoren_und_gemeinden AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal = TRUE
+    AND T.jahr = 2020
+GROUP BY S.name
+ORDER BY prozent_beschaftigte_personen_sektor_2
+DESC LIMIT 5;
+
 -- How many people were employed in the secondary sector in the municipality of Frauenfeld in Canton Thurgau in 2015?
+SELECT T.beschaftigte_personen
+FROM thurgau_beschaftigte_nach_sektoren_und_gemeinden AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal = TRUE
+    AND S.name = "Frauenfeld"
+    AND T.jahr = 2015
+    AND T.sektor = 2;
+
 -- What was the number of employees per economic sector each year in Canton Thurgau between 2017 and 2020?
--- In which municipality in Thurgau did the proportion of employees grow the most (in percentage) between 2015 and 2020?
--- What was the growth rate of each economic sector in Thurgau in percentage between 2011 and 2021?
+SELECT T.jahr, T.sektor, SUM(T.beschaftigte_personen)
+FROM thurgau_beschaftigte_nach_sektoren_und_gemeinden AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal = TRUE
+    AND T.jahr >= 2017
+    AND T.jahr <= 2020
+GROUP BY T.sektor, T.jahr;
+
+-- In which municipality in Thurgau did the number of employees grow the most between 2015 and 2020?
+SELECT S.name
+FROM thurgau_beschaftigte_nach_sektoren_und_gemeinden AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
+WHERE S.municipal = TRUE
+    AND T.jahr IN (2015, 2020)
+GROUP BY S.name
+ORDER BY (
+    100.0 * SUM(CASE WHEN T.jahr = 2020 THEN T.beschaftigte_personen ELSE 0 END) /
+    SUM(CASE WHEN T.jahr = 2015 THEN T.beschaftigte_personen ELSE 0 END)
+) DESC LIMIT 1;
+
+-- What was the growth of each economic sector in Thurgau in percentage between 2011 and 2021?
 -- In what year did the number of employees in the primary sector in Thurgau reach its peak?
 -- What is the proportion of municipalities in Thurgau where the number of employees in the primary sector was higher than in the secondary sector in 2016?
 -- What is the proportion of municipalities in Thurgau where the number of employees in the primary sector decreased between 2013 and 2018?
