bf028f55392b1781c39d1524954f4fa5c7432ffc
cmdoret <cyril.mattheydoret@gmail.com>
2023-10-09 13:35:26 +0200

    fix(A21): better field names in query outputs

--- pipelines/A21/queries.sql
+++ pipelines/A21/queries.sql
@@ -1,5 +1,5 @@
 -- Welche Gemeinde in Basel-Landschaft hatte 1995 den höchsten Stromverbrauch?
-SELECT S.name, T.endverbrauch_elektrizitaet_mwh
+SELECT S.name AS gemeinde, T.endverbrauch_elektrizitaet_mwh
 FROM basel_land_endverbrauch_von_ektrizitat_nach_gemeinde_und_jahr as T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
 WHERE S.municipal=TRUE
@@ -17,7 +17,7 @@ GROUP BY T.jahr
 ORDER BY gesamt_endverbrauch_elektrizitaet_mwh_2020 DESC;
 
 -- Welche drei Gemeinden in Basel-Landschaft hatten im Jahr 2012 den geringsten Stromverbrauch?
-SELECT S.name, T.endverbrauch_elektrizitaet_mwh
+SELECT S.name AS gemeinde, T.endverbrauch_elektrizitaet_mwh
 FROM basel_land_endverbrauch_von_ektrizitat_nach_gemeinde_und_jahr as T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
 WHERE S.municipal=TRUE
@@ -45,7 +45,7 @@ WHERE S.municipal=TRUE
 ORDER BY T.jahr DESC;
 
 -- Welche der Gemeinden Bottmingen und Birsfelden in Basel-Landschaft hatte 2018 den höchsten Stromverbrauch?
-SELECT S.name, T.endverbrauch_elektrizitaet_mwh as endverbrauch_elektrizitaet_mwh_2018
+SELECT S.name AS gemeinde, T.endverbrauch_elektrizitaet_mwh as endverbrauch_elektrizitaet_mwh_2018
 FROM basel_land_endverbrauch_von_ektrizitat_nach_gemeinde_und_jahr as T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
 WHERE S.municipal=TRUE
@@ -63,7 +63,7 @@ ORDER BY T.endverbrauch_elektrizitaet_mwh DESC
 LIMIT 1;
 
 -- Wie hoch war der höchste jemals gemessene jährliche Stromverbrauch in einer Gemeinde in Basel-Landschaft? Geben Sie auch den Namen der Gemeinde an.
-SELECT T.jahr, S.name, T.endverbrauch_elektrizitaet_mwh
+SELECT T.jahr, S.name AS gemeinde, T.endverbrauch_elektrizitaet_mwh
 FROM basel_land_endverbrauch_von_ektrizitat_nach_gemeinde_und_jahr as T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
 WHERE S.municipal=TRUE
