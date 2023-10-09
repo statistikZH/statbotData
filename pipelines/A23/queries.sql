5ff03a22e0800288a33a1c6f033da7583a26c2e2
cmdoret <cyril.mattheydoret@gmail.com>
2023-10-09 13:32:12 +0200

    fix(A23): better field names in query outputs

--- pipelines/A23/queries.sql
+++ pipelines/A23/queries.sql
@@ -1,5 +1,5 @@
 -- Welche Gemeinde in Basel-Landschaft hatte 2022 das teuerste Bauland?
-SELECT S.name
+SELECT S.name AS gemeinde
 FROM basel_land_quadratmeterpreis_wohnbauland_nach_gemeinde_und_jahr AS T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
 WHERE S.municipal=TRUE
@@ -8,7 +8,7 @@ ORDER BY T.quadratmeterpreis_chf DESC
 LIMIT 1;
 
 -- Wie viel kostete ein Quadratmeter Wohnbauland in den fünf teuersten Gemeinden von Basel-Landschaft im Jahr 2005?
-SELECT S.name, T.quadratmeterpreis_chf
+SELECT S.name AS gemeinde, T.quadratmeterpreis_chf
 FROM basel_land_quadratmeterpreis_wohnbauland_nach_gemeinde_und_jahr AS T
 JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
 WHERE S.municipal=TRUE
@@ -18,7 +18,7 @@ LIMIT 5;
 
 -- In welcher Gemeinde des Kantons Basel-Landschaft ist der Preis für Wohnbauland zwischen 2010 und 2020 am stärksten gestiegen, und um wie viel ist er gestiegen?
 SELECT
-    S.name,
+    S.name AS gemeinde,
     (
         SUM(CASE WHEN T.jahr=2020 THEN T.quadratmeterpreis_chf ELSE 0 END) -
         SUM(CASE WHEN T.jahr=2010 THEN T.quadratmeterpreis_chf ELSE 0 END)
@@ -32,7 +32,7 @@ ORDER BY preisanderung_chf DESC
 LIMIT 1;
 
 -- Welche Gemeinden in Basel-Landschaft hatten zwischen 2015 und 2019 einen Rückgang der Wohnbaulandpreise? Zeigen Sie auch die Preisänderungen an.
-SELECT T1.name, T1.preisanderung_chf
+SELECT T1.name AS gemeinde, T1.preisanderung_chf
 FROM (
     SELECT
         S.name,
@@ -51,8 +51,9 @@ WHERE T1.preisanderung_chf < 0
 ORDER BY T1.preisanderung_chf ASC ;
 
 -- Wie viele Wohnbauländer wurden 2018 in Basel-Landschaft gekauft?
-SELECT SUM(T.falle)
+SELECT SUM(T.falle) AS anzahl_wohnbaulander_basel_landschaft_2018
 FROM basel_land_quadratmeterpreis_wohnbauland_nach_gemeinde_und_jahr AS T
+JOIN spatial_unit AS S ON T.spatialunit_uid = S.spatialunit_uid
 WHERE T.jahr=2018;
 
 -- In welchem Jahr wurde die meiste Wohnfläche im Kanton Basel-Landschaft gekauft?
