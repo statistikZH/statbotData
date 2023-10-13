866d44bc877b5cdeeaaaf68eaafcf36e7c9fdf62
cmdoret <cyril.mattheydoret@gmail.com>
2023-10-13 16:23:00 +0200

    feat(A30): add questions

---
+++ pipelines/A30/queries.sql
@@ -0,0 +1,11 @@
+-- How many sales of residential building land were there in kanton Zürich in 2015 and 2021?
+-- In what year did the average price per square meter for housing land increase the most in canton Zurich?
+-- What was the average price per square meter for residential building land in canton Zurich between 1980 and 2000?
+-- In what year was the interquartile range of the price per square meter for residential building land the highest in canton Zurich?
+-- What was the relative increase in the price of housing land in canton Zurich between 1980 and 2020?
+-- When did the price of residential building land reach its peak in canton Zürich?
+-- Show me the median price of residential land in canton Zurich for each year between 1980 and 2020.
+-- Show me all statistical indicators for the price of residential land in canton Zurich for each year between 2017 and 2021.
+-- What was the 3th quartile of the price of residential land in canton Zurich in 2015?
+-- What was the 50th percentile of the price of residential land in canton Zurich in 2012 and 2014?
+-- How many sales of residential building land were there in total in kanton Zürich over the period 1990-2010?
