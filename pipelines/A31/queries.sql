10e55c62202a3f5e318cc48a1e2fe537f4b8568a
cmdoret <cyril.mattheydoret@gmail.com>
2023-10-18 22:26:44 +0200

    feat(A31): add questions

---
+++ pipelines/A31/queries.sql
@@ -0,0 +1,12 @@
+-- What was the average taxable income in the canton of Basel-Stadt in 2014?
+-- What was the average income in Matthäus, BS, 2015?
+-- What residential area in Basel-Stadt has the highest median income?
+-- How much did the average taxable income in Basel-Stadt change between 1995 and 2015?
+-- What residential area in Basel Stadt had the most income inequality in 2020 ?
+-- What were the 3 residential areas with the highest mean taxable assets in 2010?
+-- How did the mean net income and net income tax change (in percentage) between 2000 and 2020 in Clara, BS?
+-- Show me all income tax metrics about Breite, in Canton Basel-Stadt, over the period 2015-2020.
+-- What proportion of residential areas in Basel-Stadt had a median income above 100'000 CHF in 2015?
+-- Show me the Gini coefficient of net wealth for Bettingen, Iselin and Bruderholz (BS) in 2012.
+-- What was the median net wealth of the 5 residential areas of Basel-Stadt with the lowest median net income in 2015?
+-- On what year was the mean taxable income in Basel-Stadt the highest?
