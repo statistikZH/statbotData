538c6c7e273e7ec81b8345b1336c552199847cef
cmdoret <cyril.mattheydoret@gmail.com>
2023-10-20 12:35:04 +0200

    feat(A33): add pipeline

---
+++ pipelines/A33/A33.R
@@ -0,0 +1,65 @@
+# -------------------------------------------------------------------------
+# Steps: Get the data
+# input: data/const/statbot_input_data.csv
+# output: ds$data, ds$dir
+# -------------------------------------------------------------------------
+
+ds <- statbotData::create_dataset("A33")
+ds <- statbotData::download_data(ds)
+
+# -------------------------------------------------------------------------
+# Step 1 rename columns + clean values
+# -------------------------------------------------------------------------
+
+ds$postgres_export <- ds$data %>%
+  janitor::clean_names() %>%
+  dplyr::select(
+    jahr,
+    gemeinde_name,
+    geschlecht,
+    nationalitaet_bezeichnung,
+    anzahl_personen
+  ) %>%
+  dplyr::rename(
+    nationalitaet = "nationalitaet_bezeichnung",
+  )
+
+# -------------------------------------------------------------------------
+# Step 2 map to spatial units
+# -------------------------------------------------------------------------
+
+# Merge with spatialunits to get a uid for each municipality
+spatial_mapping <- ds$postgres_export %>%
+  dplyr::select(gemeinde_name, jahr) %>%
+  dplyr::distinct(gemeinde_name, jahr) %>%
+  dplyr::mutate(canton = "BS") %>%
+  statbotData::map_ds_municipalities(
+    year = jahr,
+    canton_abbr = canton,
+    municipality_name = gemeinde_name
+  ) %>%
+  dplyr::select(gemeinde_name, jahr, spatialunit_uid) %>%
+  dplyr::mutate(jahr = lubridate::year(jahr))
+
+
+ds$postgres_export <- ds$postgres_export %>%
+  dplyr::left_join(spatial_mapping, by = c("gemeinde_name", "jahr")) %>%
+  dplyr::filter(!is.na(spatialunit_uid)) %>%
+  dplyr::select(-gemeinde_name) %>%
+  janitor::clean_names()
+
+# -------------------------------------------------------------------------
+# Step: Upload dataset to postgres, test run queries, generate a sample and
+# metadata files.
+#   input:  ds$postgres_export, ds$dir/queries.sql
+#   output: ds$dir/queries.log
+# -------------------------------------------------------------------------
+
+# create the table in postgres
+statbotData::create_postgres_table(ds)
+# copy the metadata templates to the metadata files and then complete them
+statbotData::update_pipeline_last_run_date(ds)
+statbotData::update_metadata_in_postgres(ds)
+# generate sample data for the dataset from the local tibble
+statbotData::dataset_sample(ds)
+statbotData::testrun_queries(ds)
