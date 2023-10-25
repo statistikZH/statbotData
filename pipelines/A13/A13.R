6792050527ca936d7acc7dc280bea0854c78c4bb
Sabine Maennel <sabinem@users.noreply.github.com>
2023-10-25 12:14:11 +0200

    fix(A13.R): function call referencing library
    Co-authored-by: Cyril Matthey-Doret <cmdoret@mailbox.org>

--- pipelines/A13/A13.R
+++ pipelines/A13/A13.R
@@ -4,8 +4,8 @@
 # output: ds$data, ds$dir
 # -------------------------------------------------------------------------
 
-ds <- statbotData::::create_dataset("A13")
-ds <- statbotData::::download_data(ds)
+ds <- statbotData::create_dataset("A13")
+ds <- statbotData::download_data(ds)
 
 # -------------------------------------------------------------------------
 # Step: Clean the data
