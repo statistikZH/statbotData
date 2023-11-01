8cf5d73e012e4e57e07c932902ccbeed4c1460ec
cmdoret <cyril.mattheydoret@gmail.com>
2023-11-01 11:18:58 +0100

    fix: drop redundant call to create_dataset

--- scripts/upload_new_dataset.R
+++ scripts/upload_new_dataset.R
@@ -17,10 +17,7 @@
 # of your dataset
 
 data_indicator <- "A1"
-ds <- statbotData::create_dataset(data_indicator)
 
-# generate metadata template files
-statbotData::generate_metadata_templates(ds)
 # fill in the missing fields in the metadata files and copy the files
 # to `metadata_tables.csv` and `metadata_table_columns.csv`
 
@@ -34,6 +31,8 @@ pipeline_build_path <- here::here(ds$dir, paste0(data_indicator, ".R"))
 source(path)
 # check that `ds$postgres_export` is set
 ds$postgres_export
+# generate metadata template files
+statbotData::generate_metadata_templates(ds)
 
 # create a dataset sample
 statbotData::dataset_sample(ds)
@@ -49,7 +48,7 @@ statbotData::dataset_sample(ds)
 # check the dataset status
 ds_input <- statbotData::load_dataset_list() %>%
   dplyr::filter(data_indicator == ds$data_indicator)
-ds_input$status == 'uploaded'
+ds_input$status == "uploaded"
 
 # -----------------------------------------------------------------------
 # STEP 3: Update the metadata to postgres
